9fe6b6ae9408eb009dd21f134fec0d4004ab945f
Porter <plittlep-contractor@zappos.com>
2021-06-21 18:06:49 -0700

    add pg_confif symlinks

--- libraries/drivers_db_postgresql.rb
+++ libraries/drivers_db_postgresql.rb
@@ -6,6 +6,17 @@ module Drivers
       adapter :postgresql
       allowed_engines :postgres, :postgresql, :'aurora-postgresql'
       packages debian: 'libpq-dev', rhel: 'postgresql12-devel'
+
+      def setup
+        super
+        context.link '/etc/alternatives/pg_config' do
+          to '/usr/pgsql-12/bin/pg_config'
+        end
+        context.link '/usr/bin/pg_config' do
+          to '/etc/alternatives/pg_config'
+        end
+      end
+      
     end
   end
 end
